96e8f8659af3383811a469e59636fdc088b7bacd
Thales Lima <thales.bplima@gmail.com>
2021-08-16 18:45:26 -0300

    add req4

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 function cartItemClickListener(event) {
-  
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
