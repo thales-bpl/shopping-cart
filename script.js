60259750faec97bbf5dd82b11962ca7c782c5ea7
Thales Lima <thales.bplima@gmail.com>
2021-08-24 18:20:18 -0300

    recomecando projeto

--- script.js
+++ script.js
@@ -1,96 +1,8 @@
-// https://github.com/tryber/sd-014-b-project-shopping-cart
-// https://github.com/tryber/sd-014-b-project-shopping-cart/pull/19
-const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-
-const fetchProducts = async (product) => {
-  const data = await fetch(`${API_URL}${product}`)
-    .then((result) => result.json());
-  return data;
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
-}
-
-// capturando o valor de cada item no storage/cart:
-
-const sumReducer = (acc, cur) => acc + cur;
-
-const updateStoragePrices = () => {
-  const priceList = JSON.parse(localStorage.getItem('prices'));
-  const totalPrice = priceList.reduce(sumReducer, 0);
-  const totalPriceDiv = document.getElementById('total-cart-price');
-  totalPriceDiv.innerHTML = totalPrice;
-};
-
-const addPriceToStorage = (productPrice) => {
-  const priceList = JSON.parse(localStorage.getItem('prices'));
-  priceList.push(productPrice);
-  localStorage.setItem('prices', JSON.stringify(priceList));
-  updateStoragePrices();
-};
-
-const removePriceFromStorageByIndex = (index) => {
-  const priceList = JSON.parse(localStorage.getItem('prices'));
-  priceList.splice(index, 1);
-  localStorage.setItem('prices', JSON.stringify(priceList));
-  updateStoragePrices();
-};
-
-//
-// cart elements:
-
-function cartItemClickListener(event) { // obrigatória
-  const itemToBeRemoved = event.target;
-  const itemTextToBeRemoved = itemToBeRemoved.innerText;
-  itemToBeRemoved.remove();
-  const productList = JSON.parse(localStorage.getItem('products'));
-  removePriceFromStorageByIndex(productList.indexOf(itemTextToBeRemoved));
-  const newList = productList.filter((item) => item !== itemTextToBeRemoved);
-  localStorage.setItem('products', JSON.stringify(newList));
-}
-
-function cartItemClickListener2(event) {
-  const itemToBeRemoved = event.target;
-  const allLisOfItems = document.getElementsByTagName('li');
-  removePriceFromStorageByIndex(allLisOfItems.indexOf(itemToBeRemoved));
-}
-
-function createCartItemElement({ sku, name, salePrice }) { // obrigatória
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-const addToLocalStorage = (product) => {
-  const productList = JSON.parse(localStorage.getItem('products'));
-  productList.push(product);
-  localStorage.setItem('products', JSON.stringify(productList));
-};
-
-const addItemToCart = (productJson) => {
-  const productInfo = createCartItemElement({
-    sku: productJson.id,
-    name: productJson.title,
-    salePrice: productJson.price,
-  });
-  const cartItems = document.getElementsByTagName('ol')[0];
-  cartItems.appendChild(productInfo);
-  addToLocalStorage(productInfo.innerHTML);
-  addPriceToStorage(productJson.price);
-};
-
-async function getSkuFromProductItem(item) {
-  const parent = item.target.parentElement;
-  const productId = parent.querySelector('span.item__sku').innerText;
-  const data = await fetch(`https://api.mercadolibre.com/items/${productId}`)
-  .then((api) => api.json());
-  addItemToCart(data);
 }
 
 function createCustomElement(element, className, innerText) {
@@ -100,61 +12,32 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) { // obrigatória
+function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-  
+
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
-  .addEventListener('click', getSkuFromProductItem);
-  
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
   return section;
 }
 
-//
-//
-
-const importFromApiToShop = (resultSearch) => {
-  const productItemElement = createProductItemElement({
-    sku: resultSearch.id,
-    name: resultSearch.title,
-    image: resultSearch.thumbnail,
-  });
-  const sectionItens = document.querySelector('.items');
-  sectionItens.appendChild(productItemElement);
-};
-
-function createCartItemFromStorage(productSpecs) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = productSpecs;
-  li.addEventListener('click', cartItemClickListener);
-  const cartItems = document.getElementsByTagName('ol')[0];
-  cartItems.appendChild(li);
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
-const cartRender = () => {
-  if (localStorage.getItem('products') === null) {
-    localStorage.setItem('products', JSON.stringify([]));
-  } else {
-    const productList = JSON.parse(localStorage.getItem('products'));
-    productList.forEach((item) => createCartItemFromStorage(item));
-  }
-};
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
 
-const renderPrices = () => {
-  if (localStorage.getItem('prices') === null) {
-    localStorage.setItem('prices', JSON.stringify([]));
-  } else {
-    updateStoragePrices();
-  }
-};
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-window.onload = () => {
-  fetchProducts('computador')
-    .then((products) => products.results.forEach((element) => importFromApiToShop(element)));
-  cartRender();
-  renderPrices();
-};
+window.onload = () => { };
